4486bc22902187daed8ac22d5e91466518bc0f93
Gabriel Herling <gabo.gh12398@gmail.com>
2025-05-30 16:45:03 -0300

    Calculadora Básica en Consola V2.3

--- script.py
+++ script.py
@@ -2,35 +2,6 @@
 n = 0 # Variable para el número actual ingresado
 c = '' # Variable para el operador
 res = 0 # Resultado acumulado de las operaciones
-
-# Se pide el primer número y se almacena como resultado inicial
-n = int(input('Número: '))
-res = n
-
-# Bucle principal que se ejecuta hasta que el usuario ingrese '='
-while c != '=':
-    c = input('Operador: ') # Se solicita el operador
-    
-    if c == '=':
-        break # Si el operador es '=', se termina el bucle
-    elif c not in ['+', '-', '*', '/']: # Si el operador no es válido, se muestra un error y se termina
-        print('Error, operador incorrecto')
-        c = '=' # Se fuerza la salida del bucle
-    else:
-        n = int(input('Número: ')) # Si el operador es válido, se pide otro número
-
-        if c == '+':
-            res = res + n
-        elif c == '-':
-            res = res - n
-        elif c == '*':
-            res = res * n
-        elif c == '/':
-            if n == 0: # Control de error: división por cero
-                print('Error: no se puede dividir entre 0')
-                c = '=' # Se fuerza la salida del bucle
-            else:
-                res = res / n # División válida
         
 print(f'Resultado: {res}'); # Se imprime el resultado final
 n = 0  # Variable para el número actual ingresado
@@ -57,7 +28,7 @@ res = n
 
 # Bucle principal: pide operador y número hasta que se ingrese '='
 while True:
-    c = input('Operador: ')  # Se solicita el operador
+    c = input('Operador (+, -, *, / o = para salir): ')  # Se solicita el operador
 
     if c == '=':
         break  # Sale del bucle
